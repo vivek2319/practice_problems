9c53a6004328a4cf3195721c3a3b6455d6ba60a8
Vivek Kulkarni <vivek2319@gmail.com>
2024-05-08 19:00:23 +0530

    Create pascals_triangle.py

---
+++ easy/pascals_triangle.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def generate(self, numRows):
+        """
+        :type numRows: int
+        :rtype: List[List[int]]
+        """
+        result = []
+        for i in range(numRows):
+            row = [1] * (i + 1)
+            for j in range(1, i):
+                row[j] = result[i - 1][j] + result[i - 1][j - 1]
+            result.append(row)
+        return result if result else [[1]]
+
+
+        
