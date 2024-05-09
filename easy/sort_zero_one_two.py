25e63b64fc1ed7f4944245f387dcace3cfcb23cd
Vivek Kulkarni <vivek2319@gmail.com>
2024-05-09 16:46:39 +0530

    Create sort_zero_one_two.py

---
+++ easy/sort_zero_one_two.py
@@ -0,0 +1,17 @@
+class Solution:
+    def sort012(self,arr,n):
+        # code here
+        low = 0
+        mid = 0
+        high = n-1
+        
+        while mid <= high:
+            if arr[mid] == 0:
+                arr[low], arr[mid] = arr[mid], arr[low]
+                low = low + 1
+                mid = mid + 1
+            elif arr[mid] == 1:
+                mid = mid + 1
+            else:
+                arr[mid], arr[high] = arr[high], arr[mid]
+                high = high - 1
