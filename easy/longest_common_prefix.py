d7f277970f3e7614b42fe7922e41e278bfc77f69
Vivek Kulkarni <vivek2319@gmail.com>
2024-06-29 21:48:47 +0530

    Update longest_common_prefix.py

--- easy/longest_common_prefix.py
+++ easy/longest_common_prefix.py
@@ -25,12 +25,14 @@ strs[i] consists of only lowercase English letters.
 """
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        ans=""
-        v=sorted(strs)
-        first=v[0]
-        last=v[-1]
-        for i in range(min(len(first),len(last))):
-            if(first[i]!=last[i]):
-                return ans
-            ans+=first[i]
-        return ans
+        # Assumes the input list of strings is not empty
+        # The outer loop goes through each character of the first string
+        for index in range(len(strs[0])):
+            # Inner loop checks the character at the current position across all other strings
+            for string in strs[1:]:
+                # Checks if we have reached the end of any string or a character mismatch is found
+                if index >= len(string) or string[index] != strs[0][index]:
+                    # return the common longest prefix found so far
+                    return strs[0][:index]
+        # if no early return happened, the first string itself is the commmon prefix
+        return strs[0]
