8a19ecf088a16910e31655e24be6c96e8c592803
Vivek Kulkarni <vivek2319@gmail.com>
2024-08-30 20:07:31 +0530

    Create longest_univalue_path.py

---
+++ medium/longest_univalue_path.py
@@ -0,0 +1,50 @@
+"""
+Given the root of a binary tree, return the length of the longest path, where each node in the path has the same value. This path may or may not pass through the root.
+
+The length of the path between two nodes is represented by the number of edges between them.
+
+ 
+
+Example 1:
+
+
+Input: root = [5,4,5,1,1,null,5]
+Output: 2
+Explanation: The shown image shows that the longest path of the same value (i.e. 5).
+Example 2:
+
+
+Input: root = [1,4,5,4,4,null,5]
+Output: 2
+Explanation: The shown image shows that the longest path of the same value (i.e. 4).
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 104].
+-1000 <= Node.val <= 1000
+The depth of the tree will not exceed 1000.
+
+"""
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def longestUnivaluePath(self, root: Optional[TreeNode]) -> int:
+        def dfs(root):
+            if root is None:
+                return 0
+            left = dfs(root.left)
+            right = dfs(root.right)
+            left = left + 1 if root.left and root.left.val == root.val else 0
+            right = right + 1 if root.right and root.right.val == root.val else 0
+            nonlocal ans
+            ans = max(ans, left + right)
+            return max(left, right)
+        ans = 0
+        dfs(root)
+        return ans
