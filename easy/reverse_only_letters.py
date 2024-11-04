7145e962848608329a9be0ed78c0a210fe1948c7
Vivek Kulkarni <vivek2319@gmail.com>
2024-11-04 12:48:02 +0530

    Create reverse_only_letters.py

---
+++ easy/reverse_only_letters.py
@@ -0,0 +1,46 @@
+"""
+Given a string s, reverse the string according to the following rules:
+
+All the characters that are not English letters remain in the same position.
+All the English letters (lowercase or uppercase) should be reversed.
+Return s after reversing it.
+
+ 
+
+Example 1:
+
+Input: s = "ab-cd"
+Output: "dc-ba"
+Example 2:
+
+Input: s = "a-bC-dEf-ghIj"
+Output: "j-Ih-gfE-dCba"
+Example 3:
+
+Input: s = "Test1ng-Leet=code-Q!"
+Output: "Qedo1ct-eeLg=ntse-T!"
+ 
+
+Constraints:
+
+1 <= s.length <= 100
+s consists of characters with ASCII values in the range [33, 122].
+s does not contain '\"' or '\\'.
+
+"""
+
+class Solution:
+    def reverseOnlyLetters(self, s: str) -> str:
+        s = list(s)
+        left = 0
+        right = len(s) - 1
+        while left < right:
+            if not s[left].isalpha():
+                left = left + 1
+            elif not s[right].isalpha():
+                right = right - 1
+            else:
+                s[left], s[right] =  s[right] , s[left]
+                left = left + 1
+                right = right - 1
+        return ''.join(s)  
