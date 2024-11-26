a41c085b9dc28a8ccc407b8ec6752ed9cc5e560d
Vivek Kulkarni <vivek2319@gmail.com>
2024-11-26 07:48:42 +0530

    Update merge_sorted_array.py

--- easy/merge_sorted_array.py
+++ easy/merge_sorted_array.py
@@ -41,22 +41,16 @@ Follow up: Can you come up with an algorithm that runs in O(m + n) time?
 """
 class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
-        """
-        Do not return anything, modify nums1 in-place instead.
-        """
-        last = m + n -1
-
-        # merge in reverse order
-        while m > 0 and n > 0:
-            if nums1[m-1] > nums2[n-1]:
-                nums1[last] = nums1[m-1]
-                m = m - 1
+        a = m - 1
+        b = n - 1
+        write_index = m + n - 1
+    
+        while b >= 0:
+            if a >= 0 and nums1[a] > nums2[b]:
+                nums1[write_index] = nums1[a]
+                a = a - 1
             else:
-                nums1[last] = nums2[n-1]
-                n = n - 1
-            last = last - 1
-        # fill num1 with leftover elements from nums2
-        while n > 0:
-            nums1[last] = nums2[n-1]
-            n = n - 1
-            last = last - 1
+                nums1[write_index] = nums2[b]
+                b = b - 1
+            
+            write_index = write_index - 1
