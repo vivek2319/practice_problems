622b627544895756d764f2f6a66f6d5d17a281b2
Vivek Kulkarni <vivek2319@gmail.com>
2024-11-28 15:08:28 +0530

    Create shortest_distance_to_a_character.py

---
+++ easy/shortest_distance_to_a_character.py
@@ -0,0 +1,57 @@
+"""
+Given a string s and a character c that occurs in s, return an array of integers answer where answer.length == s.length and answer[i] is the distance from index i to the closest occurrence of character c in s.
+
+The distance between two indices i and j is abs(i - j), where abs is the absolute value function.
+
+ 
+
+Example 1:
+
+Input: s = "loveleetcode", c = "e"
+Output: [3,2,1,0,1,0,0,1,2,2,1,0]
+Explanation: The character 'e' appears at indices 3, 5, 6, and 11 (0-indexed).
+The closest occurrence of 'e' for index 0 is at index 3, so the distance is abs(0 - 3) = 3.
+The closest occurrence of 'e' for index 1 is at index 3, so the distance is abs(1 - 3) = 2.
+For index 4, there is a tie between the 'e' at index 3 and the 'e' at index 5, but the distance is still the same: abs(4 - 3) == abs(4 - 5) = 1.
+The closest occurrence of 'e' for index 8 is at index 6, so the distance is abs(8 - 6) = 2.
+Example 2:
+
+Input: s = "aaab", c = "b"
+Output: [3,2,1,0]
+ 
+
+Constraints:
+
+1 <= s.length <= 104
+s[i] and c are lowercase English letters.
+It is guaranteed that c occurs at least once in s.
+"""
+
+class Solution:
+    def shortestToChar(self, s: str, c: str) -> List[int]:
+        n = len(s)
+        leftDist = [n] * n
+        rightDist = [n] * n
+        res = [0] * n
+
+        dist = n
+        # left to right pass
+        for i in range(n):
+            if s[i] == c:
+                dist = 0
+            leftDist[i] = dist
+            dist = dist + 1
+        
+        dist = n
+        # right to left pass
+        for i in range(n-1, -1, -1):
+            if s[i] == c:
+                dist = 0
+            rightDist[i] = dist
+            dist = dist + 1
+        
+        # Combine Distances
+        for i in range(n):
+            res[i] = min(leftDist[i], rightDist[i])
+        
+        return res
